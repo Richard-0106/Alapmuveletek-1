da3a3bd34d0f5c5b423d7a2e8390e327dbdd1611
Ricsi <alex19950106@gmail.com>
2021-04-13 15:12:43 +0200

    Mentés

--- src/alapmuvgyak/Muveletek.java
+++ src/alapmuvgyak/Muveletek.java
@@ -1,5 +1,12 @@
 package alapmuvgyak;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 public class Muveletek extends javax.swing.JFrame {
 
     /**
@@ -206,6 +213,11 @@ public class Muveletek extends javax.swing.JFrame {
         mnuFajl.add(mnuFajlMegnyit);
 
         mnuFajlMent.setText("Ment");
+        mnuFajlMent.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuFajlMentActionPerformed(evt);
+            }
+        });
         mnuFajl.add(mnuFajlMent);
 
         mentesmaskent.setText("Mentés másként...");
@@ -281,6 +293,26 @@ public class Muveletek extends javax.swing.JFrame {
     private void btnMegoldasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMegoldasActionPerformed
         
     }//GEN-LAST:event_btnMegoldasActionPerformed
+
+    private void mnuFajlMentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuFajlMentActionPerformed
+       JFileChooser fc = new JFileChooser();
+       fc.setDialogTitle("Fájl mentése");
+       fc.setCurrentDirectory(new File("."));
+       fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        
+        int valasztottGombErteke =fc.showSaveDialog(this);
+        if (valasztottGombErteke == JFileChooser.APPROVE_OPTION){
+            File f=fc.getSelectedFile();
+         if(f.isDirectory()){
+            lblEredmeny.setText("<html>Elérés:" + f.getPath() +"<br>Könyvtár: " + f.getName()+ "</html>");
+                try {                           // vagy sima / per jel
+                    Files.write(Paths.get(f.getPath(), "\\stat.txt"), "Statisztika:".getBytes());// hova, mit
+                } catch (IOException ex) {
+                    Logger.getLogger(Muveletek.class.getName()).log(Level.SEVERE, null, ex);
+                }
+         }
+        }
+    }//GEN-LAST:event_mnuFajlMentActionPerformed
 
     /**
      * @param args the command line arguments
